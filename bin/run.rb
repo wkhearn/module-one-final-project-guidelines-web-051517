354ea07ff5a7cb72817ce8d973d2dc6c4cd0cc50
github username <github email address>
2017-05-31 10:44:27 -0400

    setting up env, etc.

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,11 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
 
-welcome
-display_players
-user.draft_player
-#do that until all players are gone
-user_team_display
-play_game
-end_game?
+puts "This is working!"
+
+# welcome
+# display_players
+# user.draft_player
+# #do that until all players are gone
+# user_team_display
+# play_game
+# end_game?
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
-require 'bundler'
-Bundler.require
+require 'bundler' #bundler is a built in class in ruby, installs required gems
+Bundler.require #runs the bundler class from above
+
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+
 require_all 'lib'
+require_all 'app/models'
