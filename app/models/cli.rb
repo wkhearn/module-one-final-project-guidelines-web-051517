6b7ab125a4494ae25d74da1925e41b6dfe7027b2
wkhearn <wkhearn@gmail.com>
2017-06-01 18:31:54 -0400

    allowing users to input player names without proper capitalization

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -1,6 +1,7 @@
 class CLI
 
   def welcome
+    puts " "
     puts "Welcome to the NBA Boxscore CLI Game!"
 
     Player.update_all(:available? => true)
@@ -17,35 +18,57 @@ class CLI
   end
 
   def display_players
+    puts " "
     puts "Please select a player from below:"
     puts "-" * 25
     puts " "
     puts "Available Players"
     puts "-" * 25
     Player.select { |player| if player.available? == true then puts player.player_name end }
-    puts "-" * 25
-    puts "Drafted Players"
-    puts "-" * 25
-    Player.select { |player| if player.available? == false then puts player.player_name end }
     puts " "
+
+    if Player.select{|player| player.available? == false}.length > 0
+      puts "-" * 25
+      puts "Drafted Players"
+      puts "-" * 25
+      Player.select { |player| if player.available? == false then puts player.player_name end }
+      puts " "
+    end
   end
 
   def user1_draft
     user = User.find(User.minimum(:id))
+    puts "-" * 25
     puts "#{user.name}, please choose a player."
     player_name = gets.chomp
-    player = Player.find_by(player_name: player_name)
-    draft = Draft.create(user_id: user.id, player_id: player.id)
-    Player.update(player.id, :available? => false)
+
+    if Player.select {|player| player.player_name.upcase == player_name.upcase}.length > 0
+      player = Player.select {|player| player.player_name.upcase == player_name.upcase}[0]
+      draft = Draft.create(user_id: user.id, player_id: player.id)
+      Player.update(player.id, :available? => false)
+    else
+      puts "Please make a valid player selection"
+      puts " "
+      cli = CLI.new()
+      cli.user1_draft
+    end
   end
 
   def user2_draft
     user = User.find(User.maximum(:id))
+    puts "-" * 25
     puts "#{user.name}, please choose a player."
     player_name = gets.chomp
-    player = Player.find_by(player_name: player_name)
-    draft = Draft.create(user_id: user.id, player_id: player.id)
-    Player.update(player.id, :available? => false)
+    if Player.select {|player| player.player_name.upcase == player_name.upcase}.length > 0
+      player = Player.select {|player| player.player_name.upcase == player_name.upcase}[0]
+      draft = Draft.create(user_id: user.id, player_id: player.id)
+      Player.update(player.id, :available? => false)
+    else
+      puts "Please make a valid player selection"
+      puts " "
+      cli = CLI.new()
+      cli.user2_draft
+    end
   end
 
 
@@ -56,7 +79,6 @@ class CLI
     puts " "
     puts "#{user1.name}'s Team: "
     puts "-" * 25
-
     user1.players.each do |player|
       puts "#{player.player_name} -- #{player.player_points} points"
     end
@@ -64,7 +86,6 @@ class CLI
     puts " "
     puts "#{user2.name}'s Team: "
     puts "-" * 25
-
     user2.players.each do |player|
       puts "#{player.player_name} -- #{player.player_points} points"
     end
@@ -74,7 +95,6 @@ class CLI
   def game_summary
     user1 = User.find(User.minimum(:id))
     user2 = User.find(User.maximum(:id))
-
     team_1_total = user1.players.map {|player| player.player_points}.sum
     team_2_total = user2.players.map {|player| player.player_points}.sum
 
@@ -98,14 +118,15 @@ class CLI
     answer = gets.chomp
     while answer != "Y" || answer != "N"
       if answer == "Y"
+        puts " "
         cli = CLI.new()
         cli.cli_play
       elsif answer == "N"
         puts "Cya next time!"
+        return
       else
         puts "Please make a valid selection"
         answer = gets.chomp
-        #loop back to beginning of end_game method
       end
     end
   end
@@ -117,17 +138,14 @@ class CLI
 
   def cli_play
     cli = CLI.new()
-
     cli.clear_table
     cli.welcome
-
-    1.times do
+    3.times do
       cli.display_players
       cli.user1_draft
       cli.display_players
       cli.user2_draft
     end
-
     # tip-off feature?
     cli.display_teams
     cli.game_summary
--- bin/run.rb
+++ bin/run.rb
@@ -2,19 +2,3 @@ require_relative '../config/environment'
 
 cli = CLI.new()
 cli.cli_play
-
-#
-# cli.clear_table
-# cli.welcome
-#
-# 1.times do
-#   cli.display_players
-#   cli.user1_draft
-#   cli.display_players
-#   cli.user2_draft
-# end
-#
-# tip-off feature?
-# cli.display_teams
-# cli.game_summary
-# cli.end_game?
